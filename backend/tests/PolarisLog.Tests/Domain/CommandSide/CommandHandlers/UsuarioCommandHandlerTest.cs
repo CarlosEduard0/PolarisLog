0958cdd903a37504e2239f119e197b48ad5d5584
Carlos Eduardo <carloseduardofox8@gmail.com>
2020-05-14 18:51:02 -0300

    test: iniciar implementação de testes unitários para comando de cadastrar usuário #7

---
+++ backend/tests/PolarisLog.Tests/Domain/CommandSide/CommandHandlers/UsuarioCommandHandlerTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PolarisLog.Domain.CommandSide.CommandHandlers;
+using PolarisLog.Domain.CommandSide.Commands.Usuario;
+using PolarisLog.Domain.Entities;
+using PolarisLog.Infra;
+using PolarisLog.Infra.Repositories;
+using PolarisLog.Tests.Helpers.Factories;
+using Xunit;
+
+namespace PolarisLog.Tests.Domain.CommandSide.CommandHandlers
+{
+    public class UsuarioCommandHandlerTest
+    {
+        private readonly Context _context;
+
+        public UsuarioCommandHandlerTest()
+        {
+            _context = ContextFactory.Create();
+        }
+
+        [Fact]
+        public async Task Handler_DeveInserirUsuarioComNomeEmailESenha()
+        {
+            var nome = "nome";
+            var email = "email@email.com";
+            var senha = "senha";
+            var senhaConfirmacao = "senha";
+            var passwordHasher = new PasswordHasher<Usuario>();
+            var command = new AdicionarNovoUsuarioCommand(nome, email, senha, senhaConfirmacao);
+            var commandHandler = new UsuarioCommandHandler(new UsuarioRepository(_context));
+
+            await commandHandler.Handle(command, CancellationToken.None);
+
+            var usuario = await _context.Usuarios.FirstOrDefaultAsync();
+            usuario.Nome.Should().Be(nome);
+            usuario.Email.Should().Be(email);
+            passwordHasher.VerifyHashedPassword(usuario, usuario.Senha, senha)
+                .Should().Be(PasswordVerificationResult.Success);
+        }
+    }
+}
---
+++ backend/tests/PolarisLog.Tests/Helpers/Factories/ContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using PolarisLog.Infra;
+
+namespace PolarisLog.Tests.Helpers.Factories
+{
+    public class ContextFactory
+    {
+        public static Context Create()
+        {
+            var options = new DbContextOptionsBuilder<Context>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            return new Context(options);
+        }
+    }
+}
