0be0629cf7c8421536bf540d80b0309c07093cb7
Carlos Eduardo <carloseduardofox8@gmail.com>
2020-05-14 18:54:33 -0300

    feat: adiciona handler de notificações #8

---
+++ backend/src/PolarisLog.Domain/Notifications/DomainNotificationHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace PolarisLog.Domain.Notifications
+{
+    public class DomainNotificationHandler : INotificationHandler<DomainNotification>
+    {
+        private List<DomainNotification> _notifications;
+
+        public DomainNotificationHandler()
+        {
+            _notifications = new List<DomainNotification>();
+        }
+        
+        public Task Handle(DomainNotification notification, CancellationToken cancellationToken)
+        {
+            _notifications.Add(notification);
+            return Task.CompletedTask;
+        }
+
+        public List<DomainNotification> ObterNotificacoes()
+        {
+            return _notifications;
+        }
+        
+        public bool TemNotificacao()
+        {
+            return _notifications.Any();
+        }
+    }
+}
