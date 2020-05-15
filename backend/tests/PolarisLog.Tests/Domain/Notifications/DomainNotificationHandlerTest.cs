7be23b23166331384584782f5701e45eed41402c
Carlos Eduardo <carloseduardofox8@gmail.com>
2020-05-14 21:14:51 -0300

    test: adiciona testes de notificação #9

---
+++ backend/tests/PolarisLog.Tests/Domain/Notifications/DomainNotificationHandlerTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Threading;
+using FluentAssertions;
+using PolarisLog.Domain.Notifications;
+using Xunit;
+
+namespace PolarisLog.Tests.Domain.Notifications
+{
+    public class DomainNotificationHandlerTest
+    {
+        [Fact]
+        public void Handler_DeveAdicionarNotificacao()
+        {
+            var chave = "nome";
+            var valor = "Nome deve possuir conteúdo";
+            var notification = new DomainNotification(chave, valor);
+            var notificationHandler = new DomainNotificationHandler();
+
+            notificationHandler.Handle(notification, CancellationToken.None);
+
+            notificationHandler.TemNotificacao().Should().Be(true);
+            notificationHandler.ObterNotificacoes().Should().HaveCount(1);
+            var notificacaoLancada = notificationHandler.ObterNotificacoes().First();
+            notificacaoLancada.Key.Should().Be(chave);
+            notificacaoLancada.Value.Should().Be(valor);
+        }
+    }
+}
