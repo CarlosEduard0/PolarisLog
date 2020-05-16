d87d6dd6a680247e2c9c3fd68c5f65bbcff5bde2
Carlos Eduardo <carloseduardofox8@gmail.com>
2020-05-16 13:45:59 -0300

    feat: adiciona migrations e sqlite

---
+++ backend/src/PolarisLog.Infra/Migrations/20200516163447_Initial.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PolarisLog.Infra.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Logs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Level = table.Column<int>(nullable: true),
+                    Descricao = table.Column<string>(nullable: true),
+                    Origem = table.Column<string>(nullable: true),
+                    ArquivadoEm = table.Column<DateTime>(nullable: true),
+                    DeletadoEm = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Logs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Nome = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Senha = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Logs");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
--- backend/src/PolarisLog.WebApi/Startup.cs
+++ backend/src/PolarisLog.WebApi/Startup.cs
@@ -29,7 +29,7 @@ namespace PolarisLog.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<Context>(options => options.UseInMemoryDatabase(Guid.NewGuid().ToString()));
+            services.AddDbContext<Context>(options => options.UseSqlite("Data Source=polarislog.db"));
             
             services.AddControllers();
             services.AddMediatR(AppDomain.CurrentDomain.Load("PolarisLog.Domain"));
