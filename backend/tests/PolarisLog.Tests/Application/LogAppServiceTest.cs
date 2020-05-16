59baf2c129eeef70011c42f474df60769a61df57
Carlos Eduardo <carloseduardofox8@gmail.com>
2020-05-16 15:42:58 -0300

    test: adiciona testes de obter todos os logs #25

--- backend/tests/PolarisLog.Tests/Application/LogAppServiceTest.cs
+++ backend/tests/PolarisLog.Tests/Application/LogAppServiceTest.cs
@@ -1,27 +1,34 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using FluentAssertions;
 using MediatR;
 using Moq;
 using PolarisLog.Application.Services;
 using PolarisLog.Application.ViewModels;
 using PolarisLog.Domain.CommandSide.Commands.Log;
 using PolarisLog.Domain.Entities;
+using PolarisLog.Domain.QuerySide.Queries.Log;
+using PolarisLog.Infra;
+using PolarisLog.Tests.Helpers.Factories;
 using Xunit;
 
 namespace PolarisLog.Tests.Application
 {
     public class LogAppServiceTest
     {
+        private readonly Context _context;
         private readonly Mock<IMediator> _mediatorMock;
 
         public LogAppServiceTest()
         {
+            _context = ContextFactory.Create();
             _mediatorMock = new Mock<IMediator>();
         }
         
         [Fact]
-        public async Task Adicionar_DeveEnviarCommandParaAdicionarLog()
+        public async Task Adicionar_DeveEnviarCommandDeAdicionarLog()
         {
             var logViewModel = new LogViewModel
             {
@@ -37,13 +44,28 @@ namespace PolarisLog.Tests.Application
         }
         
         [Fact]
-        public async Task Arquivar_DeveEnviarCommandParaArquivarLog()
+        public async Task Arquivar_DeveEnviarCommandDeArquivarLog()
         {
             var logAppService = new LogAppService(_mediatorMock.Object);
 
             await logAppService.Arquivar(Guid.NewGuid());
             
             _mediatorMock.Verify(mediator => mediator.Send(It.IsAny<ArquivarLogCommand>(), CancellationToken.None));
+        }
+        
+        [Fact]
+        public async Task ObterTodos_DeveEnviarQueryDeObterTodosOsLogs()
+        {
+            _mediatorMock
+                .Setup(mediator => mediator.Send(It.IsAny<ObterTodosOsLogsQuery>(), CancellationToken.None))
+                .Returns(async () => await Task.Run(() => new[] {new Log(Level.Verbose, "descrição", "0.0.0.0")}));
+            var logAppService = new LogAppService(_mediatorMock.Object);
+
+            var logs = await logAppService.ObterTodos();
+            
+            _mediatorMock.Verify(mediator => mediator.Send(It.IsAny<ObterTodosOsLogsQuery>(), CancellationToken.None));
+            logs.Should().HaveCount(1);
+            logs.First().CadastradoEm.Should().BeCloseTo(DateTime.Now.ToUniversalTime(), 1000);
         }
     }
 }
---
+++ backend/tests/PolarisLog.Tests/Domain/QuerySide/Queries/LogQueryHandlerTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MediatR;
+using Moq;
+using PolarisLog.Domain.Entities;
+using PolarisLog.Domain.Interfaces;
+using PolarisLog.Domain.QuerySide.Queries.Log;
+using PolarisLog.Domain.QuerySide.QueryHandlers;
+using PolarisLog.Infra;
+using PolarisLog.Infra.Repositories;
+using PolarisLog.Tests.Helpers.Factories;
+using Xunit;
+
+namespace PolarisLog.Tests.Domain.QuerySide.Queries
+{
+    public class LogQueryHandlerTest
+    {
+        private readonly Context _context;
+        private readonly ILogRepository _logRepository;
+        private readonly Mock<IMediator> _mediatorMock;
+
+        public LogQueryHandlerTest()
+        {
+            _context = ContextFactory.Create();
+            _logRepository = new LogRepository(_context);
+            _mediatorMock = new Mock<IMediator>();
+        }
+
+        [Fact]
+        public async Task HandlerObterTodos_DeveRetornarTodosOsLogsSalvosNoBanco()
+        {
+            var log1 = new Log(Level.Verbose, "descrição", "0.0.0.0");
+            var log2 = new Log(Level.Verbose, "descrição", "0.0.0.0");
+            await _context.Logs.AddRangeAsync(log1, log2);
+            await _context.SaveChangesAsync();
+            
+            var query = new ObterTodosOsLogsQuery();
+            var queryHandler = new LogQueryHandler(_logRepository);
+
+            var logs = await queryHandler.Handle(query, CancellationToken.None);
+
+            logs.Should().HaveCount(2);
+        }
+    }
+}
