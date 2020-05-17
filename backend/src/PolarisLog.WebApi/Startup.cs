b9521cccbe1c698fbe4043d2fbe463f3821d5f15
Carlos Eduardo <carloseduardofox8@gmail.com>
2020-05-17 08:52:02 -0300

    chore: adiciona swagger

--- backend/src/PolarisLog.WebApi/Startup.cs
+++ backend/src/PolarisLog.WebApi/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using PolarisLog.Infra;
 using PolarisLog.WebApi.Setup;
 
@@ -34,11 +35,39 @@ namespace PolarisLog.WebApi
             services.AddControllers();
             services.AddMediatR(AppDomain.CurrentDomain.Load("PolarisLog.Domain"));
             services.RegisterServices();
+            
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "PolarisLog API",
+                    Version = "V1",
+                    Description = "API com o objetivo de centralizar todos os registros de erros em um local, de onde " +
+                                  "podem monitorar e tomar decisões mais acertadas.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Carlos Eduardo",
+                        Email = "carloseduardofox8@gmail.com",
+                        Url = new Uri("https://eduardoazevedo.dev")
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "PolarisLog API");
+            });
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
